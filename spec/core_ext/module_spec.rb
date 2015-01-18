7f305dea2783b218cf898c4496e089ffbb3b04cc
Jakub Jirutka <jakub@jirutka.cz>
2015-01-18 21:51:27 +0100

    Split core_ext specs to multiple files

---
+++ spec/core_ext/module_spec.rb
@@ -0,0 +1,20 @@
+describe Module do
+
+  describe '#alias_class_method' do
+
+    subject(:klass) do
+      Class.new do
+        def self.salute
+          'Meow!'
+        end
+      end
+    end
+
+    it 'defines new class method that calls the old class method' do
+      klass.alias_class_method :say_hello!, :salute
+
+      expect(klass).to respond_to :say_hello!
+      expect(klass.say_hello!).to eq klass.salute
+    end
+  end
+end
--- spec/core_ext/string_spec.rb
+++ spec/core_ext/string_spec.rb
@@ -1,13 +1,3 @@
-describe Enumerable do
-
-  describe '#map_send' do
-
-    it 'sends a message to each element and collects the result' do
-      expect([1, 2, 3].map_send(:+, 3)).to eq [4, 5, 6]
-    end
-  end
-end
-
 describe String do
 
   describe '#concat' do
@@ -40,28 +30,6 @@ describe String do
           is_expected.to eq "foo\nbar"
         end
       end
-    end
-  end
-end
-
-
-describe Module do
-
-  describe '#alias_class_method' do
-
-    subject(:klass) do
-      Class.new do
-        def self.salute
-          'Meow!'
-        end
-      end
-    end
-
-    it 'defines new class method that calls the old class method' do
-      klass.alias_class_method :say_hello!, :salute
-
-      expect(klass).to respond_to :say_hello!
-      expect(klass.say_hello!).to eq klass.salute
     end
   end
 end
