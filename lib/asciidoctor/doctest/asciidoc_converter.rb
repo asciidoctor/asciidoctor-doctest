a0ed34e702ddfe472616766fd9e8c7c9bcd8ddf9
Jakub Jirutka <jakub@jirutka.cz>
2015-04-26 14:05:53 +0200

    Allow AsciidocConverter to accept any default options

--- lib/asciidoctor/doctest/asciidoc_converter.rb
+++ lib/asciidoctor/doctest/asciidoc_converter.rb
@@ -3,7 +3,8 @@ require 'asciidoctor/converter/template'
 require 'corefines'
 require 'delegate'
 
-using Corefines::Object[:blank?, :presence]
+using Corefines::Hash[:rekey, :+]
+using Corefines::Object::blank?
 
 module Asciidoctor
   module DocTest
@@ -12,42 +13,57 @@ module Asciidoctor
     # preset and validate some common parameters.
     class AsciidocConverter
 
-      attr_reader :backend_name, :converter, :template_dirs
+      # @return [Hash] the default options to be passed to Asciidoctor.
+      attr_reader :default_opts
 
       ##
-      # @param backend_name [#to_s, nil] the name of the tested backend.
+      # @param opts [Hash] the default options to be passed to Asciidoctor.
+      #        For a complete list of all available options see the
+      #        Asciidoctor's documentation
       #
-      # @param converter [Class, Asciidoctor::Converter::Base, nil]
-      #        the backend's converter class (or its instance). If not
-      #        specified, the default converter for the specified backend will
-      #        be used.
+      # @option opts :backend [#to_s, nil] the name of the tested backend.
+      # @option opts :backend_name [#to_s, nil] alias for the +:backend+.
       #
-      # @param template_dirs [Array<String>, String] path of the directory (or
-      #        multiple directories) where to look for the backend's templates.
-      #        Relative paths are referenced from the working directory.
+      # @option opts :converter [Class, Asciidoctor::Converter::Base, nil]
+      #         the backend's converter class (or its instance). When not
+      #         specified, the default converter for the specified backend is
+      #         used.
       #
-      # @param templates_fallback [Boolean] allow to fall back to using an
-      #        appropriate built-in converter when there is no required
-      #        template in the tested backend?
-      #        This is actually a default behaviour in Asciidoctor, but since
-      #        it's inappropriate for testing of custom backends, it's disabled
-      #        by default.
+      # @option opts :template_dirs [Array<String>, String] path of the
+      #         directory (or multiple directories) where to look for the
+      #         backend's templates. Relative paths are referenced from the
+      #         working directory.
+      #
+      # @option opts :templates_fallback [Boolean] allow to fall back to using
+      #         an appropriate built-in converter when there is no required
+      #         template in the tested backend?
+      #         This is actually a default behaviour in Asciidoctor, but since
+      #         it's inappropriate for testing of custom backends, it's
+      #         disabled by default.
+      #
+      # @option opts :safe [Symbol] the safe mode, one of +:unsafe+, +:safe+,
+      #         +:server+, or +:secure+. Default is +:safe+.
       #
       # @raise [ArgumentError] if some path from the +template_dirs+ doesn't
       #        exist or is not a directory.
       #
-      def initialize(backend_name: nil, converter: nil, template_dirs: [],
-                     templates_fallback: false)
+      def initialize(opts = {})
+        opts = opts.rekey(&:to_sym).rekey(:backend_name => :backend)
 
-        @backend_name = backend_name.to_s.freeze.presence
-        @converter = converter
-        @converter ||= NoFallbackTemplateConverter unless template_dirs.empty? || templates_fallback
-
-        template_dirs = Array(template_dirs).freeze
+        template_dirs = Array(opts[:template_dirs]).freeze
         template_dirs.each do |path|
           fail ArgumentError, "Templates directory '#{path}' doesn't exist!" unless Dir.exist? path
         end
-        @template_dirs = template_dirs unless template_dirs.empty?
+
+        unless template_dirs.empty?
+          opts[:template_dirs] = template_dirs
+          opts[:converter] ||= NoFallbackTemplateConverter unless opts[:templates_fallback]
+        end
+
+        opts[:safe] ||= :safe
+        opts.delete(:backend) if opts[:backend].blank?
+
+        @default_opts = opts
       end
 
       ##
@@ -55,19 +71,15 @@ module Asciidoctor
       # the tested backend.
       #
       # @param text [#to_s] the input text in AsciiDoc syntax.
-      # @param opts [Hash] options to pass to Asciidoctor.
+      # @param opts [Hash] options to pass to Asciidoctor. This will be merged
+      #        with {#default_opts} with precedence of +opts+.
       # @return [String] converted input.
       #
       def convert(text, opts = {})
-        converter_opts = {
-          safe: :safe,
-          backend: backend_name,
-          converter: converter,
-          template_dirs: template_dirs
-        }.merge(opts)
-
-        Asciidoctor.convert(text.to_s, converter_opts)
+        Asciidoctor.convert(text.to_s, @default_opts + opts)
       end
+
+      alias_method :opts, :default_opts
     end
 
     ##
--- spec/asciidoc_converter_spec.rb
+++ spec/asciidoc_converter_spec.rb
@@ -10,46 +10,47 @@ describe DocTest::AsciidocConverter do
   describe '#initialize' do
 
     context 'with defaults' do
-      subject { described_class.new }
-      it { is_expected.to have_attributes backend_name: nil, converter: nil, template_dirs: nil }
+      subject { described_class.new.opts }
+      it { is_expected.to include safe: :safe }
     end
 
     context 'with backend_name' do
-      subject { described_class.new(backend_name: 'html5') }
-      it { is_expected.to have_attributes backend_name: 'html5' }
+      subject { described_class.new(backend_name: 'html5').opts }
+      it { is_expected.to include backend: 'html5' }
 
       context 'empty string' do
-        subject { described_class.new(backend_name: '') }
-        it { is_expected.to have_attributes backend_name: nil }
+        subject { described_class.new(backend_name: '').opts }
+        it { is_expected.to_not include :backend }
       end
     end
 
     context 'with template_dirs' do
       include FakeFS::SpecHelpers
 
-      subject { described_class.new(template_dirs: template_dirs) }
+      subject { described_class.new(template_dirs: template_dirs).opts }
       let(:template_dirs) { ['/tmp/html5'] }
 
       before { FileUtils.mkpath template_dirs[0] }
 
       context 'that exists' do
         it do
-          is_expected.to have_attributes(
+          is_expected.to include(
             template_dirs: template_dirs,
             converter: DocTest::NoFallbackTemplateConverter
           )
         end
 
-        context 'and templates_fallback = true' do
-          subject { described_class.new(template_dirs: template_dirs, templates_fallback: true) }
-          it { is_expected.to have_attributes template_dirs: template_dirs, converter: nil }
+        context 'and templates_fallback is true' do
+          subject { described_class.new(template_dirs: template_dirs, templates_fallback: true).opts }
+          it { is_expected.to include template_dirs: template_dirs }
+          it { is_expected.to_not include :converter }
         end
 
         context 'and custom converter' do
-          subject { described_class.new(template_dirs: template_dirs, converter: converter) }
+          subject { described_class.new(template_dirs: template_dirs, converter: converter).opts }
           let(:converter) { Asciidoctor::Converter::TemplateConverter }
 
-          it { is_expected.to have_attributes template_dirs: template_dirs, converter: converter }
+          it { is_expected.to include template_dirs: template_dirs, converter: converter }
         end
       end
 
