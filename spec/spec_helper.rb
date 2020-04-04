a7db64bf2388c8bcded0eadb429ba2d2b239ec8e
Jakub Jirutka <jakub@jirutka.cz>
2020-04-04 23:19:41 +0200

    Fix test coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
+unless RUBY_ENGINE == 'jruby'
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
 require 'corefines'
 require 'rspec/collection_matchers'
 require 'simplecov'
